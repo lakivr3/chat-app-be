6d1dd2be0f2d5e22a19f523a20cfa249db694ff4
Lazar Zdravkovic <lakivr14@gmail.com>
2025-03-11 21:49:47 +0100

    done

---
+++ src/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from "express"
+import { checkAuth, login, logout, signup,updateProfile } from "../controllers/auth.contoller.js"
+import { protectRoute } from "../middleware/auth.middleware.js"
+
+const route = express.Router()
+
+route.post('/signup',signup)
+route.post('/login',login)
+route.post('/logout',logout)
+
+
+route.put('/update-profile',protectRoute, updateProfile)
+route.get('/check',protectRoute,checkAuth)
+
+
+
+export default route
